12039b13d6288998bf75b232df6ace43374f267d
Sillywillyphil97 <63065566+Sillywillyphil97@users.noreply.github.com>
2022-02-15 16:53:43 -0600

    Update node-cmd-sender.sh

--- ADP-Cluster/node-cmd-sender.sh
+++ ADP-Cluster/node-cmd-sender.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-#-----------------------------------------------
-#
-#   Run the same command across all worker nodes.
-#
-#   Created on: 15 FEB 2022
-#   Created By: Michael Dearing
-#
-#   Version Notes:
-#   1.0 : 12/02/2022 - 
-#
-#-----------------------------------------------
 # This script is used to restart a node within a MicroFocus ADP CDF ver. 2021 (Calls scripts that only exist within this k8s install)
 # The normal k8s process of restarting a node is too aggressive for the kafka pieces within TransformationHub.
 # The kube-stop and kube-restart scripts included within this k8s install are also too aggressive when pod eviction. 
@@ -33,6 +22,7 @@ nodes=(
 for node in ${nodes[@]}; do
     if ping -c 1 $node &>/dev/null; then
         cmd="$node '$*'"
+        echo "$node:"
         if ssh $cmd 2>/dev/null; then
             echo "CMD:'$*' completed successfully on $node"
         else
