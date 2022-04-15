5fed0a23e1e71a3c32de9336a9fdcbec72aa277b
Mike <63065566+Sillywillyphil97@users.noreply.github.com>
2022-04-15 09:34:32 -0500

    Update container-restart.ps1
    New way of killing processes and the wrappers subprocesses.

--- SmartConnectors/container-restart.ps1
+++ SmartConnectors/container-restart.ps1
@@ -6,10 +6,18 @@
 #   Created By: Michael Dearing
 #
 #   Version Notes:
-#   2.0 : 09/03/2022 - New way of killing processes and the wrappers subprocesses.
+#   2.5 : 06/04/2022 - New way of killing processes and the wrappers subprocesses.
 #
 #-----------------------------------------------
-# User editable area: 
+# User editable area:
+$winc = "arc_Connector_2"
+$wincb = "F:\arcsight\connectors\connector_2\current"
+$sysmon = "arc_Container_5"
+$sysmonb = "F:\arcsight\connectors\Container 5\current"
+$powershell = "arc_connector_3"
+$psb = "F:\arcsight\connectors\connector_3\current"
+
+# DO NOT EDIT 
 $ageout = "-1"
 $currentDate = Get-Date
 $oldfile = $currentDate.AddDays($ageout)
@@ -19,6 +27,9 @@ $logpath = "\logs"
 $adpath = "\user\agent\agentdata"
 $runpath = "\run"
 $tail = "\logs\agent.out.wrapper.log"
+ 
+
+
 # Force stopping the service before checking and allowing user to kill individual processes related to the service. 
 function fkproc {
     Param([int]$ppid)
@@ -26,65 +37,78 @@ function fkproc {
     Get-CimInstance Win32_Process | Where-Object { $_.ParentProcessId -eq $ppid } | ForEach-Object { fkproc $_.ProcessId }
     Stop-Process -Id $ppid -Force
 }
-$selection = Read-Host -Prompt 'Enter (1)-WINC   (2)-Sysmon: '
+
+# User selection
+$selection = Read-Host -Prompt 'Enter (1)-WINC   (2)-Sysmon   (3)-PowerShell'
 Write-Host ""
 if ($selection -eq '1'){
-    $service = "arc_Container_1"
-    $base = 'F:\ArcSight Containers\Container 1\current'
-} else {
-    $service = "arc_Container_2"
-    $base = 'F:\ArcSight Containers\Container 2\current'
+    $service = $winc
+    $base = $wincb
+} 
+elseif ($selection -eq '2'){
+    $service = $sysmon
+    $base = $sysmonb
+} 
+else {
+    $service = $powershell
+    $base = $psb
 }
+
+# Stopping process in separate window then waiting 10 seconds before force killing process.
+Write-Output "Stopping $service. Please wait..."
+Start-Process PowerShell.exe -ArgumentList "-noexit", "-command Stop-Service $service; exit" -WindowStyle Minimized
+Start-Sleep -Seconds 10
 $mypid = 0
 $myProcessToKill = (Get-CimInstance -Class Win32_Service -Filter "Name LIKE '$service'" | Select-Object -ExpandProperty ProcessId)
 
 if ($myProcessToKill -eq "") {
     Write-Output "$service is not running."
+    Write-Output ""
 } else {
     $mypid = (Get-CimInstance -Class Win32_Service -Filter "Name LIKE '$service'" | Select-Object -ExpandProperty ProcessId)
     Write-Output "The $service PID is: $mypid"
     if ($mypid -gt 1) {
         Write-Output "Killing $service and all of it's child processes..."
+        Write-Output ""
         fkproc $mypid
     }
 }
+
 Write-Output ""
 Write-Output "$service is now stopped.."
 Write-Output ""
 
 # Removes stale cache files out of the \current\user\agent\agentdata\
 Write-Output "Cleaning stale cache files."
-Write-Output ""
 Get-ChildItem -Path $base$adpath | Where-Object { $_.LastWriteTime -lt $oldfile } | Remove-Item
-Start-Sleep -Seconds 2
+Get-ChildItem -Path $base$adpath | Where-Object { $_.Name.EndsWith(".size.dflt") } | Remove-Item
+
+
+Start-Sleep -Seconds 1
 
 # Removes HeapDumps from log dir
 Write-Output "Removing HeapDump Files"
-Write-Output ""
 Get-ChildItem -Path $base$logpath | Where-Object { $_.Name.StartsWith("HeapDump") } | Remove-Item
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 1
 
 # Removes Threaddump files from log dir
 Write-Output "Removing Threaddump files"
-Write-Output ""
 Get-ChildItem -Path $base$logpath | Where-Object { $_.Name.StartsWith("Thread") } | Remove-Item
-Start-Sleep -Seconds 2
 
 # Removes any lock files that may be present from killing the service.
 Write-Output "Removing agent.lock files."
-Write-Output ""
 Get-ChildItem -Path $base$runpath | Where-Object { $_.Name.EndsWith(".lock") } | Remove-Item
-Start-Sleep -Seconds 2
 
-
-Write-Output "Starting Connector Service."
 Write-Output ""
-# Lanching the services MMC so connector can be started. 
-Start-Service -Name $service
+Write-Output "Starting $service. Please wait..."
+Write-Output ""
+# Lanching the services MMC so connector can be started.
+Write-Output "Starting Service. Please wait..."
+Start-Process PowerShell.exe -ArgumentList "-noexit", "-command Start-Service $service; exit" -WindowStyle Minimized
+# Start-Service -Name $service
 
 # Tailing agent.out.wrapper.log file to make sure connector comes up fully
-Write-Output ""
 Write-Output "Tailing agent.out.wrapper.log"
 Write-Output ""
-start-sleep -Seconds 2
-Get-Content -Path $base$tail -Wait -Tail 20  
+start-sleep -Seconds 1
+Get-Content -Path $base$tail -Wait -Tail 1
